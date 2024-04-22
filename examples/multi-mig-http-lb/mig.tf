88bfc1f35d6c35eb0c6b02bb49f856ae12454683
Alex-2605 <39106585+Alex-2605@users.noreply.github.com>
2024-04-22 12:57:23 -0600

    Update mig.tf

--- examples/multi-mig-http-lb/mig.tf
+++ examples/multi-mig-http-lb/mig.tf
@@ -81,11 +81,11 @@ module "mig2" {
 
   autoscaling_enabled =   true
   autoscaling_cpu = [{
-    target            = 0.08
+    target            = 0.06
   }]
   min_replicas    = 1
   max_replicas    = 10
-  cooldown_period = 20
+  cooldown_period = 17
 }
 
 
